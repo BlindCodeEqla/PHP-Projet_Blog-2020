06c20bf1a8de65fabba33a0a60e444d7eeb75722
idefix007 <katia@bbcom.be>
2020-09-23 19:25:25 +0200

    Ajout du détail de l'article

--- commentaires.php
+++ commentaires.php
@@ -1,6 +1,19 @@
 <?php
 include "config.php";
 
+$reponse=$bdd->prepare('SELECT *, DATE_FORMAT(date_creation,"%d/%m/%Y")  AS date_fr FROM billet WHERE id = ?');
+$reponse->execute(array($_GET['id']));
+$donnees = $reponse->fetch();
+
+?>
+<p>
+    <strong>Titre</strong>: <?php echo $donnees['titre'] ; ?><br>
+    <strong>Contenu</strong>: <?php echo $donnees['contenu'] ; ?><br>
+    <strong>Date</strong>: <?php echo $donnees['date_fr'] ; ?>
+</p>
+<?php
+$reponse->closeCursor();
+
 $reponse=$bdd->prepare('SELECT *, DATE_FORMAT(date_commentaire,"%d/%m/%Y")  AS date_fr FROM commentaire WHERE id_billet= ? ORDER BY date_commentaire');
 $reponse->execute(array($_GET['id']));
 
