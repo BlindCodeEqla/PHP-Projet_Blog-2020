0cca8ee44a48b3d7f7ccfa6bbae8164c0dcd7eb7
Katia <katia@bbcom.be>
2020-11-12 12:51:01 +0100

    - suppression fichiers de Sophie

--- sophie/deconnexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start(); // on poursuit la session sur la page
-$_SESSION= array(); // on vide toutes les variables de session: tableau complètement vide
-session_destroy(); // on détruit la session
-header("Location: index.php"); // on redirige l'utilisateur
