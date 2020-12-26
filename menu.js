93e016acf811b330b5ab5b4982193e01c4a02b02
nanbari <36733240+nanbari@users.noreply.github.com>
2020-12-26 13:02:55 +0100

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-    $('#menuToggle').click(function(){
-        $("#contenu_nav").toggle();
-        
-    });
-});
+var nav = document.getElementById("NavBar");
+var btn = document.getElementById("menuToggle");
+var message_erreur = document.querySelectorAll(".message_erreur");
+
+btn.addEventListener("click", function () {
+    nav.classList.toggle('nav_ouvrir');
+  });
+
+  
+function validateForm() {
+  var x = document.forms["monForm"]["pseudo"].value;
+  if (x == "") {
+    message_erreur.style.display="block";
+    return false;
+  }
+}
